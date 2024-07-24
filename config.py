4ff845c55f779f9b0aa68e25ca05344405ce3542
plplpopp <94629194+plplpopp@users.noreply.github.com>
2024-07-24 10:56:31 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,22 +2,15 @@ open_update = True
 source_file = "demo.txt"
 final_file = "result.txt"
 favorite_list = [
-    "广东珠江",
     "CCTV-1",
     "CCTV-5",
     "CCTV-5+",
     "CCTV-13",
-    "广东体育",
-    "广东卫视",
-    "大湾区卫视",
-    "浙江卫视",
-    "湖南卫视",
-    "翡翠台",
 ]
 open_online_search = True
 favorite_page_num = 5
 default_page_num = 3
-urls_limit = 15
+urls_limit = 8
 open_sort = True
 response_time_weight = 0.5
 resolution_weight = 0.5
@@ -34,4 +27,4 @@ subscribe_urls = [
 open_multicast = True
 region_list = ["all"]
 open_proxy = True
-open_driver = False
+open_driver = True

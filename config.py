8c1d9e7da1a1191ac08a3e56131443d8f70bb24a
plplpopp <94629194+plplpopp@users.noreply.github.com>
2024-09-29 20:32:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,8 @@ ffmpeg_time = 10
 # key: 地区，在http://tonkiang.us网站上搜索的关键词
 # value: 订阅url，在https://github.com/xisohi/IPTV-Multicast-source中找自己想要的
 search_dict = {
-    "安徽": "https://mirror.ghproxy.com/raw.githubusercontent.com/plplpopp/TVbox/refs/heads/main/anhui.txt"
+    "安徽": "https://mirror.ghproxy.com/raw.githubusercontent.com/plplpopp/TVbox/refs/heads/main/anhui.txt",
+    "湖南": "https://mirror.ghproxy.com/raw.githubusercontent.com/plplpopp/TVbox/refs/heads/main/hunan.txt"
 }
 # 在http://tonkiang.us网站上搜索的源的页数
 search_page_num = 5

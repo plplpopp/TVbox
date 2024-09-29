51b5326d1c136f0310241f4fef7f68a0dc1d0bd8
plplpopp <94629194+plplpopp@users.noreply.github.com>
2024-09-30 07:09:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -23,20 +23,20 @@ ffmpeg_time = 10
 # key: 地区，在http://tonkiang.us网站上搜索的关键词
 # value: 订阅url，在https://github.com/xisohi/IPTV-Multicast-source中找自己想要的
 search_dict = {
-    "安徽": "https://mirror.ghproxy.com/raw.githubusercontent.com/plplpopp/TVbox/refs/heads/main/anhui.txt",
-    "湖南": "https://mirror.ghproxy.com/raw.githubusercontent.com/plplpopp/TVbox/refs/heads/main/hunan.txt"
+    "安徽": "https://mirror.ghproxy.com/https://raw.githubusercontent.com/plplpopp/TV/refs/heads/main/speedtest/txt/anhui.txt",
+    "湖南": "https://mirror.ghproxy.com/https://raw.githubusercontent.com/plplpopp/TV/refs/heads/main/speedtest/txt/hunan.txt"
 }
 # 在http://tonkiang.us网站上搜索的源的页数
-search_page_num = 5
+search_page_num = 10
 # url关键词黑名单
 url_keywords_blacklist = []
 # 忽略的关键词，比如在demo.txt中配置广东珠江,但在订阅中只有广东珠江高清,就需要忽略掉"高清"
 search_ignore_key = ["高清", "4K"]
 # crawl_type的默认值为1-只爬取http://tonkiang.us上组播源；2-只爬取crawl_urls中配置的网站；3-全部
-crawl_type = "1"
+crawl_type = "3"
 # 收集其他大佬url中的直播源
 crawl_urls = [
-    "https://mirror.ghproxy.com/raw.githubusercontent.com/plplpopp/collect-tv-txt/refs/heads/main/merged_output.txt"
+    "https://mirror.ghproxy.com/https://raw.githubusercontent.com/plplpopp/TV/refs/heads/main/speedtest/zubo.txt"
 ]
 # ipv6源检测有效性的代理地址，用于不支持ipv6网络的主机，若网络支持ipv6，这里填空""
 ipv6_proxy = "http://www.ipv6proxy.net/go.php?u=" #此代理用于github，国内不一定能用
